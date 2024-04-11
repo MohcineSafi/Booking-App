4708d569ece8af1f46a87c14141cd5f51839b92a
MohcineSafi <safi.mouhcine02@gmail.com>
2024-04-11 01:00:59 +0000

    Feat:Install Tailwindcss for Vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,29 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      {" "}
-      <h1>TEST</h1>{" "}
-    </>
+    <div>
+      <header>
+        <img src="airbnb-1.svg" alt="" />
+        <h1>TEST</h1>{" "}
+      </header>
+      <div>
+        <h2>Anywhere</h2>
+      </div>
+      <div>Any guests</div>
+      <div>
+        <h2>any week</h2>
+      </div>
+      <button>
+        <span>Search</span>
+        <img src="search.svg" alt="" />
+      </button>
+      <div>
+        <img src="BurgerMenu.svg" alt="" />
+      </div>
+      <div>
+        <img src="user.svg" alt="" />
+      </div>
+    </div>
   );
 }
 
