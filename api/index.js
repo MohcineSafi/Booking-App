8046bc9d0d7ce402c3c14fecfb8b06bd0f2da01e
MohcineSafi <safi.mouhcine02@gmail.com>
2024-04-12 00:50:45 +0000

    Feat:Add Profile Route

--- api/index.js
+++ api/index.js
@@ -70,4 +70,18 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
+app.get("/api/profile", (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
+    });
+  } else {
+    res.json(null);
+  }
+});
+
 app.listen(4000);
