2a692a0552763b49c3cc1ca0d47bff518282b160
MohcineSafi <safi.mouhcine02@gmail.com>
2024-04-15 00:36:30 +0100

    Feat:Implement endpoint for uploading image by link, downloading image, and uploading to S3

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
 const User = require("./models/User.js");
 const { default: mongoose } = require("mongoose");
 
@@ -14,6 +15,7 @@ const jwtSecret = "fasefraw4r5r3wq45wdfgw34twdfg";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
@@ -86,6 +88,21 @@ app.get("/api/profile", (req, res) => {
 
 app.post("/api/logout", (req, res) => {
   res.cookie("token", "").json(true);
+});
+
+app.post("/api/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image({
+    url: link,
+    dest: "/tmp/" + newName,
+  });
+  const url = await uploadToS3(
+    "/tmp/" + newName,
+    newName,
+    mime.lookup("/tmp/" + newName)
+  );
+  res.json(url);
 });
 
 app.listen(4000);
