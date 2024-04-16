6411d7a5ab99d04bafa39384f5fd2fed3b27b1e8
MohcineSafi <safi.mouhcine02@gmail.com>
2024-04-17 00:37:42 +0100

    Feat:Update api GET user-places  route in index

--- api/index.js
+++ api/index.js
@@ -157,4 +157,13 @@ app.post("/api/places", (req, res) => {
   });
 });
 
+app.get("/api/user-places", (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await Place.find({ owner: id }));
+  });
+});
+
 app.listen(4000);
